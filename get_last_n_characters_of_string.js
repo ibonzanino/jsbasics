52d3c630e4fbd728792b46f9a1e3c9f8a4a5c3ca
ibonzanino <bonzaninoigor@gmail.com>
2022-03-06 14:36:19 -0300

    basic challenges js

---
+++ get_last_n_characters_of_string.js
@@ -0,0 +1,9 @@
+// Write a function that takes a string as argument
+// Extract the last 3 characters from the string
+// Return the result
+function myFunction(a, n) {
+
+    return a.slice(n);
+}
+
+console.log(myFunction('1234', -3));
---
+++ name_archive.js
@@ -0,0 +1,5 @@
+function nameS(str) {
+    return console.log(str.toLowerCase().split(' ').join('_') + '.js');
+}
+
+nameS('Return the next higher prime number')
